77bffda1407a5af6fa0cb2402373fc9a4761b418
Olexandr Kovalyshyn <kovalyshyn@gmail.com>
2020-10-28 00:15:34 +0200

    Добавляє код ДЗ

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import "./css/styles.css";
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const refs = {
+    body: document.querySelector('body'),
+}
+
+console.log(refs.body);
